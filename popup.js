66a9a8fba897785ef68e49261fd6bd99ca1212b9
ise-bachelor <129273697+ise-bachelor@users.noreply.github.com>
2025-06-29 09:14:31 +0000

    カメラ映像を出力

--- popup.js
+++ popup.js
@@ -1,3 +1,11 @@
-document.getElementById("btn").addEventListener("click", () => {
-  alert("拡張ボタンが押されました！");
+document.getElementById("startBtn").addEventListener("click", async () => {
+  const video = document.getElementById("video");
+
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    video.srcObject = stream;
+  } catch (err) {
+    console.error("カメラの起動に失敗しました:", err);
+    alert("カメラが使えませんでした。");
+  }
 });
