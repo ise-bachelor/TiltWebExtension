013a186f64920b755a4ee6c01aca15257309e345
ise-bachelor <129273697+ise-bachelor@users.noreply.github.com>
2025-06-29 09:54:40 +0000

    cssが適用されないエラーを改善

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 document.documentElement.style.transform = "rotate(5deg)";
 document.documentElement.style.transformOrigin = "top left";
 const faceVideo = document.getElementById("faceVideo");
-faceVideo.removeAttribute("style");
+faceVideo.style.all = "unset"
 faceVideo.style.position = "fixed";
 faceVideo.style.bottom = "3px";
 faceVideo.style.right = "3px";
