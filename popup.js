5ba88614fdd5c44172c5c26959958526b02623e4
ise-bachelor <129273697+ise-bachelor@users.noreply.github.com>
2025-06-29 09:23:37 +0000

    起動時に読み込まれてカメラがエラーになる問題を解決

--- popup.js
+++ popup.js
@@ -1,11 +1,14 @@
-document.getElementById("startBtn").addEventListener("click", async () => {
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("startBtn");
   const video = document.getElementById("video");
 
-  try {
-    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-    video.srcObject = stream;
-  } catch (err) {
-    console.error("カメラの起動に失敗しました:", err);
-    alert("カメラが使えませんでした。");
-  }
+  btn.addEventListener("click", async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      video.srcObject = stream;
+    } catch (err) {
+      console.error("カメラの起動に失敗しました:", err);
+      alert("カメラが使えませんでした。");
+    }
+  });
 });
