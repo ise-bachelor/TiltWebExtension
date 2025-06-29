038ef0995b51cec155f8f94e5331af1844fd6763
ise-bachelor <129273697+ise-bachelor@users.noreply.github.com>
2025-06-29 09:52:24 +0000

    ビデオの位置を変更

--- content.js
+++ content.js
@@ -1,3 +1,8 @@
 document.documentElement.style.transform = "rotate(5deg)";
 document.documentElement.style.transformOrigin = "top left";
+const faceVideo = document.getElementById("faceVideo");
+faceVideo.removeAttribute("style");
+faceVideo.style.position = "fixed";
+faceVideo.style.bottom = "3px";
+faceVideo.style.right = "3px";
 
