cdfc03647ed5035893a19f5440c6bf18719cd123
ise-bachelor <129273697+ise-bachelor@users.noreply.github.com>
2025-07-14 08:32:44 +0000

    a

--- popup.js
+++ popup.js
@@ -1,15 +1,21 @@
 document.getElementById("startBtn").addEventListener("click", async () => {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-  // await chrome.scripting.executeScript({
-  //   target: { tabId: tab.id },
-  //   files: [
-  //     "libs/face_mesh.js",
-  //     "libs/face_mesh_solution_packed_assets_loader.js",
-  //     "libs/face_mesh_solution_simd_wasm_bin.js",
-  //     "libs/face_mesh_solution_wasm_bin.js",
-  //     "libs/camera_utils.js",
-  //     "content.js"
-  //   ]
-  // });
+  await chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: [
+      "libs/face_mesh.js",
+      "libs/face_mesh_solution_packed_assets_loader.js",
+      "libs/face_mesh_solution_packed_assets.data",
+      "libs/face_mesh_solution_simd_wasm_bin.data",
+      "libs/face_mesh_solution_simd_wasm_bin.js",
+      "libs/face_mesh_solution_simd_wasm_bin.wasm",
+      "libs/face_mesh_solution_wasm_bin.js",
+      "libs/face_mesh_solution_wasm_bin.wasm",
+      "libs/face_mesh.binarypb",
+      "libs/face_mesh.js",
+      "libs/camera_utils/camera_utils.js",
+      "content.js"
+    ]
+  });
 });
