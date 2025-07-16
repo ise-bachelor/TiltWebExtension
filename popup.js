0c85e9a4ebf70927f9b2e612ecc7e84c675f0bf6
ise-bachelor <129273697+ise-bachelor@users.noreply.github.com>
2025-07-16 09:11:35 +0000

    a

--- popup.js
+++ popup.js
@@ -4,6 +4,7 @@ document.getElementById("startBtn").addEventListener("click", async () => {
   await chrome.scripting.executeScript({
     target: { tabId: tab.id },
     files: [
+      "libs/face_mesh.js",
       "libs/face_mesh_solution_wasm_bin.js",
       "libs/camera_utils.js",
       "content.js"
