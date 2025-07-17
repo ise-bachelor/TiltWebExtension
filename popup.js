e35bdec06a0080e56346fc7aea8eb22964e0c4fe
ise-bachelor <129273697+ise-bachelor@users.noreply.github.com>
2025-07-17 09:09:25 +0000

    a

--- popup.js
+++ popup.js
@@ -5,11 +5,11 @@ document.getElementById("startBtn").addEventListener("click", async () => {
     target: { tabId: tab.id },
     files: [
       "libs/face_mesh_solution_packed_assets_loader.js",
-      "libs/face_mesh.js",
       "libs/face_mesh_solution_wasm_bin.js",
       "libs/face_mesh_solution_simd_wasm_bin.js",
-      "libs/camera_utils.js",
-      "content.js"
+      "libs/face_mesh.js",
+      "content.js",
+      "libs/camera_utils.js"
     ]
   });
 });
